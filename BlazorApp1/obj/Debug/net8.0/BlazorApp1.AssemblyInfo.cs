9d97df6f5c38de9d3a742627f9c1d12d3b3d6cbe
Erik <esandqvist04@gmail.com>
2025-03-18 15:37:31 +0100

    simple database

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21d8f54ae2ecf4aa2f53149d8c9ca80e46e5927c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
