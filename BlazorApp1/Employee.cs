21d8f54ae2ecf4aa2f53149d8c9ca80e46e5927c
Erik <esandqvist04@gmail.com>
2025-03-18 14:22:14 +0100

    init

---
+++ BlazorApp1/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp1
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Country { get; set; }
+        public int Phone { get; set; }
+    }
+}
